0b0a837d6fcab212691aefc62d56ad1ea92b7cf7
Ryan <kochy@hotmail.co.uk>
2024-03-26 16:01:56 +0000

    feat: added menu to page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,12 @@
 "useClient";
 import Header from "./Components/Common/Header";
+import MainMenu from "./Components/Common/MainMenu";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <MainMenu />
+    </>
+  );
 }
