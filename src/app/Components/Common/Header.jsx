c9d9711384c492e67a1027a304cb934322b26ece
Ryan <kochy@hotmail.co.uk>
2024-03-26 15:59:45 +0000

    changed header styling

--- src/app/Components/Common/Header.jsx
+++ src/app/Components/Common/Header.jsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import headerBackground from "../../../../public/SandboxChessImage.png";
-const headerTheme = require("../../../../tailwind.config.js").theme.header;
 
 const Header = () => {
   return (
-    <div className={`${headerTheme.layout} ${headerTheme.spacing}`}>
+    <div className="flex flex-col items-center mt-10 mb-20">
       <Link href="/">
         <Image src={headerBackground} alt="Sandbox Chess" />
       </Link>
