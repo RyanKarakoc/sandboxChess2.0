808dfc6c48d53a029e6798d993076b4d2d3ef9a7
Ryan <kochy@hotmail.co.uk>
2024-03-26 19:13:00 +0000

    feat: added pieces to board

--- src/app/Components/Common/board/board.jsx
+++ src/app/Components/Common/board/board.jsx
@@ -1,7 +1,60 @@
 "use client";
 
+import BlackBishop from "../pieces/bishop/BlackBishop";
+import WhiteBishop from "../pieces/bishop/WhiteBishop";
+import BlackKing from "../pieces/king/BlackKing";
+import WhiteKing from "../pieces/king/WhiteKing";
+import BlackKnight from "../pieces/knight/BlackKnight";
+import WhiteKnight from "../pieces/knight/WhiteKnight";
+import BlackPawn from "../pieces/pawn/BlackPawn";
+import WhitePawn from "../pieces/pawn/WhitePawn";
+import BlackQueen from "../pieces/queen/BlackQueen";
+import WhiteQueen from "../pieces/queen/WhiteQueen";
+import BlackRook from "../pieces/rook/BlackRook";
+import WhiteRook from "../pieces/rook/WhiteRook";
+
+const tileColor = require("./tileColour");
+
+const Piece = ({ type }) => {
+  const renderPiece = () => {
+    switch (type) {
+      case "♟︎":
+        return <BlackPawn />;
+      case "♙":
+        return <WhitePawn />;
+      case "♜":
+        return <BlackRook />;
+      case "♖":
+        return <WhiteRook />;
+      case "♞":
+        return <BlackKnight />;
+      case "♘":
+        return <WhiteKnight />;
+      case "♝":
+        return <BlackBishop />;
+      case "♗":
+        return <WhiteBishop />;
+      case "♚":
+        return <BlackKing />;
+      case "♔":
+        return <WhiteKing />;
+      case "♛":
+        return <BlackQueen />;
+      case "♕":
+        return <WhiteQueen />;
+      default:
+        return null;
+    }
+  };
+  return (
+    <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
+      {renderPiece()}
+    </div>
+  );
+};
+
 const Board = () => {
-  const initialBoardState = [
+  const boardState = [
     ["♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"],
     ["♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙"],
     ["", "", "", "", "", "", "", ""],
@@ -11,25 +64,24 @@ const Board = () => {
     ["♟︎", "♟︎", "♟︎", "♟︎", "♟︎", "♟︎", "♟︎", "♟︎"],
     ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"],
   ];
-
-  const tileColor = (rowIndex, colIndex) => {
-    return (rowIndex + colIndex) % 2 !== 0 ? "bg-green-700" : "bg-yellow-100";
-  };
+  boardState.reverse();
 
   return (
     <div className="flex justify-center items-center mb-20">
       <div className="border-4 border-solid border-black rounded-md p-10 bg-amber-200">
         <div className="border-4 border-black">
-          {initialBoardState.map((row, rowIndex) => (
+          {boardState.map((row, rowIndex) => (
             <div key={rowIndex} className="flex">
               {row.map((tile, colIndex) => (
                 <div
-                  key={tile}
+                  key={colIndex}
                   className={`relative h-10 md:h-20 w-10 md:w-20 select-none ${tileColor(
                     rowIndex,
                     colIndex
                   )}`}
-                ></div>
+                >
+                  {tile && <Piece type={tile} />}
+                </div>
               ))}
             </div>
           ))}
