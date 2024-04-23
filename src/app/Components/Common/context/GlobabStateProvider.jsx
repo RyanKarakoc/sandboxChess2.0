02e1210715b71de34a6b9904f1ee944ebbc14a41
Ryan <kochy@hotmail.co.uk>
2024-04-23 11:33:35 +0100

    feat: created globalState for start / end tiles

---
+++ src/app/Components/Common/context/GlobabStateProvider.jsx
@@ -0,0 +1,33 @@
+"use client";
+import { createContext, useContext, useState } from "react";
+
+// Create a context to hold your global state
+const GlobalStateContext = createContext();
+
+// Create a provider component to wrap your application
+export const GlobalStateProvider = ({ children }) => {
+  const [startTile, setStartTile] = useState({});
+  const [endTile, setEndTile] = useState({});
+  const [moveNumber, setMoveNumber] = useState(0);
+
+  const colourToMove = moveNumber % 2 === 0 ? "white" : "black";
+
+  return (
+    <GlobalStateContext.Provider
+      value={{
+        startTile,
+        setStartTile,
+        endTile,
+        setEndTile,
+        moveNumber,
+        setMoveNumber,
+        colourToMove,
+      }}
+    >
+      {children}
+    </GlobalStateContext.Provider>
+  );
+};
+
+// Custom hook to access the global state and dispatch function
+export const useGlobalState = () => useContext(GlobalStateContext);
