bef3e1c9fb779dddecb64f4545fdcebf58257a84
Ryan <kochy@hotmail.co.uk>
2024-05-07 21:36:02 +0100

    docs: added documentation for hasPieceBeenCaptured

--- utils/pieces/hasPieceCaptured.js
+++ utils/pieces/hasPieceCaptured.js
@@ -4,18 +4,24 @@ const { blackPieces } = require("./blackPieces");
 const { whitePieces } = require("./whitePieces");
 
 const hasPieceCaptured = (endTile, currentBoardState, colourToMove) => {
+  // Calculating the row index in the board state array
   const boardStateRow = endTile.row - boardOffset;
+  // Finding the column index in the board state array using column reference
   const boardStateColumn = columnRef.indexOf(endTile.column);
 
+  // Selecting the appropriate pieces array based on the current color to move
   const pieces = colourToMove === "white" ? blackPieces : whitePieces;
 
+  // Loop through the pieces array to check if any piece matches the one at the specified position
   for (const piece in pieces) {
+    // If the piece at the specified position matches, return true (piece has been captured)
     if (
       currentBoardState[boardStateRow][boardStateColumn].piece === pieces[piece]
     ) {
       return true;
     }
   }
+  // If no piece is found matching the specified criteria, return false (no piece captured)
   return false;
 };
 
